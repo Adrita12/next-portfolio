f03788d16dd9741d6fcd9a005b83eac142d78ed5
Adrita12 <adubhattacharjee@gmail.com>
2026-03-28 20:23:07 +0530

    removed the black hole in mobile view

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-150px] md:top-[-340px] h-full w-full left-0 z-[-10] object-cover"
+        className="hidden md:block rotate-180 absolute top-[-340px] h-full w-full left-0 z-[-10] object-cover"
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
